635fff9c389e55e5ec0e5774ba51de99259d7b3a
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-04-27 00:02:05 +0545

    arrow fxn

--- 05 preloader animation/app.js
+++ 05 preloader animation/app.js
@@ -3,6 +3,6 @@ const loader = document.getElementById('preloader')
 window.addEventListener('load', load => {
 
     window.removeEventListener('load', load, false)              
-    setTimeout(function(){loader.style.display = 'none'},1100)
+    setTimeout( () => loader.style.display = 'none' , 1100)
   
   },false)
