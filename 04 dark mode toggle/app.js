29becb188462a41c7999997fec6335f7bda58b17
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-04-28 16:37:11 +0545

    complete

--- 04 dark mode toggle/app.js
+++ 04 dark mode toggle/app.js
@@ -1,18 +1,23 @@
 const cont = document.getElementById("cont")
 const tog = document.getElementById("tog")
-
+const mode = document.getElementById("mode")
 
 
 cont.addEventListener('click', () => {
-    if(cont.classList.contains('.active')){
-        document.body.backgroundColor = "var(--darkgradient)"
+   
+
+    if(!cont.classList.contains('active')){
+        document.body.style.background = "var(--darkgradient)"
+        mode.innerText = "Dark Mode"
+            cont.classList.toggle("active")
+            tog.classList.toggle("active")
+
     }
-    cont.classList.toggle("active")
-    tog.classList.toggle("active")
+    else{
+        document.body.style.background = "var(--lightgradient)"
+        mode.innerText = "Light Mode"
+            cont.classList.remove("active")
+            tog.classList.remove("active")
 
+    }
 })
-
-
-
-
-
