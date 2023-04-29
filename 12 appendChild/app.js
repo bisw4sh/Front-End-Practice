bfba0206b50164069e3313b6de64c43e2ed19d6d
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-04-29 11:28:43 +0545

    script was executing before html rendered

---
+++ 12 appendChild/app.js
@@ -0,0 +1,14 @@
+const span = document.createElement("span")
+const text = document.createTextNode("Hello, this is biswash")
+
+// btn.addEventListner('click', () => {
+//     console.log('Hello')
+
+const appending = () => {
+
+    span.appendChild(text)
+    document.body.appendChild(span)
+}  
+// },false )
+
+
