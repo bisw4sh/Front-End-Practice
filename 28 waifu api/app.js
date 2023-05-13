8937aa48fc2ab63380c4e3695d8acc8f46aed281
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-14 00:21:07 +0545

    initial commit

---
+++ 28 waifu api/app.js
@@ -0,0 +1,50 @@
+const logJSONData = async(type, category) => {
+    const response = await fetch(`https://api.waifu.pics/${type}/${category}`);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+
+// Define the data
+const data = {
+    "sfw" : [ 'waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug','awoo',
+        'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold',
+        'nom', 'bite', 'glomp','slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe'],
+    "nsfw" :['waifu', 'neko', 'trap', 'blowjob']
+};
+
+// Function to populate the sub-category drop down menu
+function populatecategory() {
+    const category = document.getElementById("type").value;
+    const categoryDropdown = document.getElementById("category");
+
+    // Clear the sub-category drop down menu
+    categoryDropdown.innerHTML = "";
+
+    // Add the default option
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.text = "--Please choose an option--";
+    categoryDropdown.add(defaultOption);
+
+    // Add the options based on the selected category
+    data[category].forEach(function(value) {
+        const option = document.createElement("option");
+        option.value = value;
+        option.text = value;
+        categoryDropdown.add(option);
+    });
+}
+
+// Call the populateSubCategory function when the category is changed
+document.getElementById("type").addEventListener("change", function() {
+    populatecategory();
+});
+
+const typesel = document.querySelector('#type')
+const categorysel = document.querySelector('#category')
+document.querySelector('.submit').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(typesel.value, categorysel.value)
+    logJSONData(typesel.value,categorysel.value)
+})
+
