349d6c6e3d74e236d068a2cbb17da4470afa1d5c
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-14 09:07:33 +0545

    refactoring to async iife

--- 28 waifu api/app.js
+++ 28 waifu api/app.js
@@ -1,10 +1,6 @@
 const logJSONData = async(type, category) => {
-    const response = await fetch(`https://api.waifu.pics/${type}/${category}`);
-    const jsonData = await response.json();
-
-    //setting the image in DOM
-    const ima_ge = document.querySelector('.image-response')
-    ima_ge.src = jsonData['url']
+    const response = await fetch(`https://api.waifu.pics/${type}/${category}`)
+    const jsonData = await response.json()
     return jsonData['url']
   }
 
@@ -22,36 +18,40 @@ const data = {
 
 // Function to populate the sub-category drop down menu
 function populatecategory() {
-    const category = document.getElementById("type").value;
-    const categoryDropdown = document.getElementById("category");
+    const category = document.getElementById("type").value
+    const categoryDropdown = document.getElementById("category")
 
     // Clear the sub-category drop down menu
-    categoryDropdown.innerHTML = "";
+    categoryDropdown.innerHTML = ""
 
     // Add the default option
-    const defaultOption = document.createElement("option");
-    defaultOption.value = "";
-    defaultOption.text = "Select";
-    categoryDropdown.add(defaultOption);
+    const defaultOption = document.createElement("option")
+    defaultOption.value = ""
+    defaultOption.text = "Select"
+    categoryDropdown.add(defaultOption)
 
     // Add the options based on the selected category
     data[category].forEach(function(value) {
-        const option = document.createElement("option");
-        option.value = value;
-        option.text = value;
-        categoryDropdown.add(option);
+        const option = document.createElement("option")
+        option.value = value
+        option.text = value
+        categoryDropdown.add(option)
     });
 }
 
 // Call the populateSubCategory function when the category is changed
 document.getElementById("type").addEventListener("change", function() {
-    populatecategory();
+    populatecategory()
 });
 
 const typesel = document.querySelector('#type')
 const categorysel = document.querySelector('#category')
+const image = document.querySelector('.image-response')
+
 document.querySelector('.submit').addEventListener('click', (e) => {
-    e.preventDefault()
-logJSONData(typesel.value,categorysel.value)
+e.preventDefault();
+(async () => {
+    image.src = await logJSONData(typesel.value,categorysel.value)
+})()
 })
 
