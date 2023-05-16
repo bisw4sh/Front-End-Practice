cd8672d8a34794ff9888ece843027a762c58a587
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-16 08:53:35 +0545

    works iff cors blocked

--- 30 mywebsite QRcode/app.js
+++ 30 mywebsite QRcode/app.js
@@ -1,7 +1,20 @@
 const imageElement = document.querySelector('.imgCon')
 
 window.onload = async() => {
-  const blob = await fetch(`https://qrtag.net/api/qr_12.png?url=https://www.biswashdhungana.com.np`, { mode: "no-cors"})
+  try{
+
+  const response = await fetch(`https://qrtag.net/api/qr_12.png?url=https://www.biswashdhungana.com.np`, {redirect: 'follow'})
+  let blob = await response.blob()
+  console.log(blob)
   const imageUrl = await URL.createObjectURL(blob)
-  imageElement.src = await imageUrl;
+  imageElement.src = await imageUrl  
+
+  }
+  catch(err){
+
+    console.log(err)
+    console.log("Turn OFF CORS policy to view JS generated")
+    document.querySelector('.imgCon').style.display = 'none'
+
+  }
 }
